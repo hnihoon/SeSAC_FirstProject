2121917e54c44e2f12e7fa7751121b468a221aba
kdh <kdh462601@gmail.com>
2025-02-28 10:08:11 +0900

    [SSD-86] chore: git switch를 하기위한 commit

--- backend/src/main/java/com/timepaper/backend/domain/user/auth/AuthController.java
+++ backend/src/main/java/com/timepaper/backend/domain/user/auth/AuthController.java
@@ -1,8 +1,11 @@
 package com.timepaper.backend.domain.user.auth;
 
+import com.timepaper.backend.domain.user.auth.dto.EmailverificationCheckRequestDto;
 import com.timepaper.backend.domain.user.auth.dto.EmailverificationRequestDto;
 import com.timepaper.backend.domain.user.auth.dto.SignupDto;
+import com.timepaper.backend.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class AuthController {
 
   private final AuthService authService;
 
-  @PostMapping("/auth/email-verification-codes")
-  public boolean emailverification(@RequestBody EmailverificationRequestDto dto) {
-    return authService.emailverification(dto);
+  @PostMapping("/email-verification-codes")
+  public ResponseEntity<ApiResponse<Boolean>> emailverification(
+      @RequestBody EmailverificationRequestDto dto) {
+    return ResponseEntity.ok(ApiResponse.ok(
+        authService.emailverification(dto)
+    ));
   }
 
-  @PostMapping("/email-verification-codes/validate")
-  public boolean checkEmailVerificationCode(@RequestBody EmailverificationRequestDto dto) {
-    return authService.checkEmailVerificationCode(dto);
+
+  @PostMapping("/auth/email-verification-codes/validate")
+  public ResponseEntity<Boolean> checkEmailVerificationCode(
+      @RequestBody EmailverificationCheckRequestDto dto) {
+    boolean result = authService.checkEmailVerificationCode(dto);
+    return ResponseEntity.ok(result);
   }
 
   @PostMapping("/signup")
